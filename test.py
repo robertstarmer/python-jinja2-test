e767442eb67f4c992ffeb2953967d9a3c7fec4bd
Robert Starmer <starmer@cisco.com>
2013-01-13 21:47:47 +0000

    First attempt, yaml includes variants of all template elements

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from jinja2 import Template, Environment, FileSystemLoader
 
 test = yaml.load(open("test.yaml"))
 env = Environment(loader=FileSystemLoader('./'))
-template = env.get_template('test.ji')
+template = env.get_template('site.pp.jinja')
 
 template.render(test)
 
